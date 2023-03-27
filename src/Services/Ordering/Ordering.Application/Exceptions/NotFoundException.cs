d17ef29165c0cee834b53a0bc4921b2878f0f2e1
Carlos <carlosbolorino@hotmail.com>
2023-03-27 19:33:37 +0200

    Commands queries and validations

---
+++ src/Services/Ordering/Ordering.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.Serialization;
+
+namespace Ordering.Application.Exceptions
+{
+    [Serializable]
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException()
+        {
+        }
+
+        public NotFoundException(string name, object key) : base($"Entity {name} ({key}) not found.")
+        {
+            
+        }
+
+        public NotFoundException(string? message) : base(message)
+        {
+        }
+
+        public NotFoundException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected NotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation.Results;
+using System.ComponentModel;
+using System.Runtime.Serialization;
+
+namespace Ordering.Application.Exceptions
+{
+    [Serializable]
+    public class ValidationException : ApplicationException
+    {
+        public ValidationException() : base("One or more validation errors ocurred")
+        {
+        }
+
+        public ValidationException(IEnumerable<ValidationFailure> failures) : this()
+        {
+            Errors = failures.GroupBy(e => e.PropertyName, e => e.ErrorMessage)
+                .ToDictionary(g => g.Key, g => g.ToArray());
+        }
+
+        public ValidationException(string? message) : base(message)
+        {
+        }
+
+        public ValidationException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected ValidationException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        public Dictionary<string, string[]> Errors { get; } = new Dictionary<string, string[]>();
+    }
+}
