9d99a1e1b0a65e2150deab5acdaaa6ee9d6a714a
Carlos <carlosbolorino@hotmail.com>
2023-03-29 18:30:42 +0200

    add repos

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,13 +1,10 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Ordering.Application.Contracts.Infrastructure;
 using Ordering.Application.Contracts.Persistence;
 using Ordering.Application.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Ordering.Infrastructure.Persistence;
 
 namespace Ordering.Infrastructure
 {
@@ -15,14 +12,14 @@ namespace Ordering.Infrastructure
     {
         public static void AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
         {
-            //services.AddDbContext<OrderContext>(options =>
-            //    options.UseSqlServer(configuration.GetConnectionString("OrderingConnectionString")));
+            services.AddDbContext<OrderContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("OrderingConnectionString")));
 
-            //services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
-            //services.AddScoped<IOrderRepository, OrderRepository>();
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+            services.AddScoped<IOrderRepository, OrderRepository>();
 
-            //services.Configure<EmailSettings>(c => configuration.GetSection("EmailSettings"));
-            //services.AddTransient<IEmailService, EmailService>();
+            services.Configure<EmailSettings>(c => configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailService, EmailService>();
 
         }
     }
