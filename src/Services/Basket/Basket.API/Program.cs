268472e2a7ed9492f9617c436aaa1a1f67909d23
Carlos <carlosbolorino@hotmail.com>
2023-04-04 18:47:53 +0200

    Consume events

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -5,6 +5,29 @@ using MassTransit;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddOptions<MassTransitHostOptions>()
+            .Configure(options =>
+            {
+                options.WaitUntilStarted = true;
+                options.StartTimeout = TimeSpan.FromSeconds(10);
+                options.StopTimeout = TimeSpan.FromSeconds(30);
+            });
+
+builder.Services.AddMassTransit(opt =>
+{
+    opt.UsingRabbitMq((ctx, cfg) =>
+    {
+        cfg.Host(builder.Configuration["EventBusSettings:HostAddress"], c =>
+        {
+
+            c.Username("guest");
+            c.Password("guest");
+        });
+
+    });
+});
+
+
 // Add services to the container.
 builder.Services.AddStackExchangeRedisCache(opt =>
 {
@@ -17,26 +40,6 @@ builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
     (o => o.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!));
 
 builder.Services.AddAutoMapper(typeof(Program));
-builder.Services.AddMassTransit(opt =>
-{
-    opt.UsingRabbitMq((ctx, cfg) =>
-    {
-        cfg.Host(builder.Configuration["EventBusSettings:HostAddress"], "/", c => {
-
-            c.Username("guest");
-            c.Password("guest");
-        });
-        
-    });
-});
-
-builder.Services.AddOptions<MassTransitHostOptions>()
-            .Configure(options =>
-            {
-                options.WaitUntilStarted = true;
-                options.StartTimeout = TimeSpan.FromSeconds(10);
-                options.StopTimeout = TimeSpan.FromSeconds(30);
-            });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,12 +1,44 @@
+using EventBus.Messages.Common;
+using MassTransit;
+using Ordering.API.EventBusConsumer;
 using Ordering.API.Extensions;
+using Ordering.API.Mapping;
 using Ordering.Application;
-using Ordering.Application.Mappings;
 using Ordering.Infrastructure;
 using Ordering.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddOptions<MassTransitHostOptions>()
+            .Configure(options =>
+            {
+                options.WaitUntilStarted = true;
+                options.StartTimeout = TimeSpan.FromSeconds(10);
+                options.StopTimeout = TimeSpan.FromSeconds(30);
+            });
+builder.Services.AddMassTransit(opt =>
+{
+    opt.AddConsumer<BasketCheckoutConsumer>();
+    opt.UsingRabbitMq((ctx, cfg) =>
+    {
+        cfg.Host(builder.Configuration["EventBusSettings:HostAddress"], c =>
+        {
+            c.Username("guest");
+            c.Password("guest");
+        });
+        cfg.ReceiveEndpoint(EventBusConstants.BasketCheckoutQueue, c =>
+        {
+            c.ConfigureConsumer<BasketCheckoutConsumer>(ctx);
 
-builder.Services.AddAutoMapper(typeof(MappingProfile).Assembly);
+        });
+
+    });
+});
+
+
+
+builder.Services.AddAutoMapper(typeof(OrderingProfile).Assembly);
+builder.Services.AddScoped<BasketCheckoutConsumer>();
+
 // Add services to the container.
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
@@ -17,12 +49,11 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.MigrateDatabase<OrderContext>((context, services) =>
+await app.MigrateDatabase<OrderContext>(async (context, services) =>
 {
     var logger = services.GetService<ILogger<OrderContextSeed>>();
-    OrderContextSeed
-        .SeedAsync(context, logger)
-        .Wait();
+    await OrderContextSeed
+        .SeedAsync(context, logger);
 });
 
 // Configure the HTTP request pipeline.
